ec4a3fb2c98eb0f0747d1bd9991c6006d1334f56
Lincy-Lin <153726372+Lincy-Lin@users.noreply.github.com>
2026-04-25 14:46:35 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_sobel user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
@@ -47,4 +47,3 @@ module tb ();
   );
 
 endmodule
-`default_nettype wire
